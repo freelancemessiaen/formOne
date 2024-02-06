a1e9f0f1eb9188ea20cff7bbbae7ad659a638097
freelancemessiaen <153985208+freelancemessiaen@users.noreply.github.com>
2024-02-06 09:15:16 +0100

    mise à jour

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from "react-hook-form"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { register, handleSubmit, getValues, formState: {errors} } = useForm();
+
+  function submit(values) {
+    console.log(values);
+  }
+
+  // console.log(getValues());
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="d-flex flex-row justify-content-center align-items-center" 
+    style={{backgroundColor: "#fefefe", height: "100vh", width: "100%"}}> 
+
+    <form onSubmit={handleSubmit(submit)}>
+      <div className="d-flex flex-column mb-20">
+        <label className="mb-5" htmlFor="age">Age</label>
+        <input {...register("age" ,{
+          valueAsNumber: true
+        })} id="age" type="number" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button className="btn btn-primary">Save</button>
+      {/* {errors?.age && <p>{errors.age.message}</p>} */}
+    </form>
+    </div>
   )
 }
-
 export default App
